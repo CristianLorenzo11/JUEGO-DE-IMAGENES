db696c755061718bf933bbdec2318db7af4eb527
Cristian Emmanuel Lorenzo <134024355+CristianLorenzo11@users.noreply.github.com>
2024-10-19 10:55:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const images = [
-    'image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg',
-    'image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg',
+    'image1.jpeg', 'image2.jpg', 'image3.jpg', 'image4.jpg',
+    'image1.jpeg', 'image2.jpg', 'image3.jpg', 'image4.jpg',
     'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg',
     'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg',
 ];
